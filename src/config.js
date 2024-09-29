491a0ffc0a52566b7fd3ccf8211c2b54d9a2b215
surajbahugunalc <surajb@linecraft.ai>
2024-09-29 19:02:14 +0530

    abc

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = 'http://127.0.0.1:5000/';
 // const API_BASE_URL = 'http://43.204.130.30:7000/';
-const API_BASE_URL = 'http://3.108.53.173:7000/';
+const API_BASE_URL = 'https://api.reveu.ai/';
 
 export default API_BASE_URL;
